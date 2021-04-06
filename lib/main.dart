2dbf9b3d3f9b5ac85fabc5fe665ab27c04283f7c
sxhao <sxhao@uwaterloo.ca>
2021-04-06 14:36:54 -0400

    final commit

---
+++ lib/main.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+
+  List<Icon> scoreKeeper = [];
+
+  void checkAnswer(bool button) {
+
+    if (quizBrain.getQuestionAnswer() == button){
+      scoreKeeper.add(
+          Icon(Icons.check, color: Colors.green)
+      );
+    }
+    else {
+      scoreKeeper.add(
+          Icon(Icons.close, color: Colors.red)
+      );
+    }
+
+    if(quizBrain.isLastQuestion()) {
+      Alert(
+        title: 'Quiz Completed!',
+        context: context,
+        type: AlertType.info,
+        desc: 'You have completed the quiz. Press button to restart.',
+        buttons: [
+          DialogButton(
+              child: Text('Continue'),
+              onPressed: () {
+                setState(() {
+                  Navigator.pop(context);
+                  quizBrain.resetQuestionCount();
+                  scoreKeeper.removeRange(0, scoreKeeper.length);
+                });
+              },
+          ),
+        ],
+      ).show();
+    }
+
+    quizBrain.nextQuestion();
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+                setState(() {
+                  checkAnswer(true);
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+                setState(() {
+                  checkAnswer(false);
+                });
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        ),
+      ],
+    );
+  }
+}
+
--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,33 +1,12 @@
-![App Brewery Banner](https://github.com/londonappbrewery/Images/blob/master/AppBreweryBanner.png)
+import 'package:flutter/cupertino.dart';
 
+import 'question.dart';
 
-# Quizzler ❓
+class QuizBrain{
 
-## Our Goal
+  int _questionCount = 0;
 
-In this tutorial we will be reviewing Stateful and Stateless Widgets as well as learning about the fundamental building blocks of Object Oriented Programming (OOP) - Classes and Objects. 
-
-
-## What you will create
-
-We’re going to make a quiz app that tests your general knowledge. As one of the most popular types of apps on the app stores, you can create your own quiz for other people to enjoy!
-
-![Finished App](https://github.com/londonappbrewery/Images/blob/master/quizzler-demo.gif)
-
-## What you will learn
-
-- Modularising your code into separate classes.
-- Dart classes and objects.
-- Using class constructors.
-- Extracting Widgets to refactor your code.
-- private and public modifiers in Dart.
-- How to use Dart lists.
-- The difference between var, const and final.
-
-## Code Snippet for Project
-
-```
-Question('Some cats are actually allergic to humans', true),
+  List<Question> _questions = [
     Question('You can lead a cow down stairs but not up stairs.', false),
     Question('Approximately one quarter of human bones are in the feet.', true),
     Question('A slug\'s blood is green.', true),
@@ -52,10 +31,39 @@ Question('Some cats are actually allergic to humans', true),
     Question(
         'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
         true),
+  ];
 
-```
+  void nextQuestion() {
+    if(_questionCount < _questions.length - 1) {
+      _questionCount++;
+    }
+    print(_questionCount);
+  }
+  bool isLastQuestion() {
+    if(_questionCount >= _questions.length - 1) {
+      int temp = _questionCount;
+      nextQuestion();
+      if(temp == _questionCount) {
+        return true;
+      }
+      else
+        {
+          _questionCount--;
+        }
+    }
+    return false;
+  }
+  String getQuestionText() {
+    return _questions[_questionCount].questionText;
+  }
+  bool getQuestionAnswer() {
+    return _questions[_questionCount].questionAnswer;
+  }
+  int getListLength() {
+    return _questions.length;
+  }
+  void resetQuestionCount() {
+    _questionCount = 0;
+  }
 
-
->This is a companion project to The App Brewery's Complete Flutter Development Bootcamp, check out the full course at [www.appbrewery.co](https://www.appbrewery.co/)
-
-![End Banner](https://github.com/londonappbrewery/Images/blob/master/readme-end-banner.png)
+}
